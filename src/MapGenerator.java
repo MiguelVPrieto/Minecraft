cff0e680692d80b99e2d9490c6619812847c9d04
MiguelVPrieto <miguelprieto.espanha@gmail.com>
2024-08-25 20:30:36 -0300

    Fixed Height Map

--- src/MapGenerator.java
+++ src/MapGenerator.java
@@ -2,17 +2,5 @@ public class MapGenerator {
     public static void main(String[] args) {
         HeightMap heightmap = new HeightMap();
 
-        for (int y = 0; y < heightmap.length; y++) {
-            for (int x = 0; x < heightmap.width; x++) {
-                double value = heightmap.heightMap[x][y];
-                if (value < 0.3) {
-                    // Water
-                } else if (value < 0.6) {
-                    // Grassland
-                } else {
-                    // Mountain
-                }
-            }
-        }
     }
 }
